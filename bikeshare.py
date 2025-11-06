4eb32390610c28fdfb811544cfd574225e7bc801
Patrick Feuerer <patrick.feuerer@porsche.de>
2025-11-06 14:23:19 +0000

    Added descriptive comments to bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -1,7 +1,9 @@
+# import neccessary tools.
 import time
 import pandas as pd
 import numpy as np
 
+# map cities to data files
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
